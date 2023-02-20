30b06bd89b65784ff96d84de792904c0210a5459
mitul99 <md.mitul.hossen99@gmail.com>
2023-02-21 02:36:32 +0600

    hero_slider fully responsive

--- src/Components/HeroSlider/HeroSlider.jsx
+++ src/Components/HeroSlider/HeroSlider.jsx
@@ -8,7 +8,7 @@ import hero3 from "../../assets/bg-highlight3.png";
 const HeroSlider = () => {
 
     return (
-        <div className="text-black h-screen">
+        <div className="text-black h-screen overflow-hidden">
             <div className="h-full lg:flex">
                 {/* lg_hero_Slider */}
                 <div className="hidden lg:block w-[45%] bg-orange-800 relative overflow-hidden">
@@ -26,36 +26,36 @@ const HeroSlider = () => {
                 <div className="hero lg:w-[55%]">
                     <div className='hero-wrap'>
                         <div className="w-full absolute slider1">
-                            <p className='block top-[150px] absolute pl-16 text-[73px] font-extrabold text-[#fa4738] hero_animate'>Travel & <span className='block mt-[-20px] text-[#fa4738] hero_stroke'>Adventure</span></p>
+                            <p className='block top-[150px] absolute pl-16 sm:text-[73px] text-[50px] font-extrabold text-[#fa4738] hero_animate'>Travel & <span className='block mt-[-20px] text-[#fa4738] hero_stroke'>Adventure</span></p>
 
-                            <p className='text-white pt-7 pl-16 w-5/6 absolute top-[350px] hero_details_animate'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, mollitia animi? Ipsum ratione ullam numquam soluta aliquam quia vitae voluptatibus!
+                            <p className='text-white pt-7 pl-16 w-5/6 lg:w-3/6 absolute top-[350px] hero_details_animate'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, mollitia animi? Ipsum ratione ullam numquam soluta aliquam quia vitae voluptatibus!
                             </p>
 
                             {/* button div */}
-                            <div className='pl-16 mt-16 absolute top-[420px] hero_btn_animate'>
+                            <div className='pl-16 mt-16 absolute sm:top-[420px] top-[480px] hero_btn_animate'>
                                 <button type="submit" className='mr-8 bg-[#fa4738] px-8 py-3 text-[18px] font-semibold text-white border-2 border-[#fa4738]
                             hover:bg-transparent
                             hover:text-[#fa4738] duration-300'>View Adventure</button>
 
-                                <button type="submit" className='px-8 py-3 font-semibold border-2 border-[#fa4738] text-[#fa4738] text-[18px] hover:bg-[#fa4738] hover:text-white duration-300'>Take A Tour</button>
+                                <button type="submit" className='px-8 py-3 font-semibold border-2 border-[#fa4738] text-[#fa4738] text-[18px] hover:bg-[#fa4738] hover:text-white duration-300 mt-4 sm:mt-0'>Take A Tour</button>
 
 
                             </div>
                         </div>
 
                         <div className="w-full absolute slider2 ">
-                            <p className='block top-[150px] absolute pl-16 text-[73px] font-extrabold text-[#fa4738] hero_animate'>Move The <span className='block mt-[-20px] text-[#fa4738] hero_stroke'>Earth</span></p>
+                            <p className='block top-[150px]  absolute pl-16 sm:text-[73px] text-[50px] font-extrabold text-[#fa4738] hero_animate'>Move The <span className='block mt-[-20px] text-[#fa4738] hero_stroke'>Earth</span></p>
 
-                            <p className='text-white pt-7 pl-16 w-5/6 absolute top-[350px] hero_details_animate'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, mollitia animi? Ipsum ratione ullam numquam soluta aliquam quia vitae voluptatibus!
+                            <p className='text-white pt-7 pl-16 w-5/6 lg:w-3/6 absolute top-[350px] hero_details_animate'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, mollitia animi? Ipsum ratione ullam numquam soluta aliquam quia vitae voluptatibus!
                             </p>
 
                             {/* button div */}
-                            <div className='pl-16 mt-16 absolute top-[420px] hero_btn_animate'>
+                            <div className='pl-16 mt-16 absolute sm:top-[420px] top-[480px] hero_btn_animate'>
                                 <button type="submit" className='mr-8 bg-[#fa4738] px-8 py-3 text-[18px] font-semibold text-white border-2 border-[#fa4738]
                             hover:bg-transparent
                             hover:text-[#fa4738] duration-300'>View Adventure</button>
 
-                                <button type="submit" className='px-8 py-3 font-semibold border-2 border-[#fa4738] text-[#fa4738] text-[18px] hover:bg-[#fa4738] hover:text-white duration-300'>Take A Tour</button>
+                                <button type="submit" className='px-8 py-3 font-semibold border-2 border-[#fa4738] text-[#fa4738] text-[18px] hover:bg-[#fa4738] hover:text-white duration-300 mt-4 sm:mt-0'>Take A Tour</button>
                             </div>
                         </div>
 
